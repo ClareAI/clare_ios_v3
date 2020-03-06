fec693eba272fdb76c4947c115b64c6da692787c
vernalclareai <zekai@clare.ai>
2020-03-06 11:10:25 +0800

    Initial commit

---
+++ Example/Clare/CLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CLAppDelegate.h
+//  Clare
+//
+//  Created by workpanda on 03/06/2020.
+//  Copyright (c) 2020 workpanda. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/Clare/CLViewController.h
@@ -0,0 +1,13 @@
+//
+//  CLViewController.h
+//  Clare
+//
+//  Created by workpanda on 03/06/2020.
+//  Copyright (c) 2020 workpanda. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CLViewController : UIViewController
+
+@end
