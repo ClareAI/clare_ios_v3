9fb260e0331731808369ab7f219106a4ff86f7cc
vernalclareai <zekai@clare.ai>
2020-03-18 23:48:50 +0800

    Initial commit

---
+++ Example/Clare/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Clare
+//
+//  Created by workpanda on 03/18/2020.
+//  Copyright (c) 2020 workpanda. All rights reserved.
+//
+
+@import UIKit;
+#import "CLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CLAppDelegate class]));
+    }
+}
