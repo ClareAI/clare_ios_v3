5e398ca152640d7a83833b9d9d19a6198de61eb2
vernalclareai <zekai@clare.ai>
2020-03-19 01:27:28 +0800

    Initial commit

---
+++ Example/Clare/CLViewController.m
@@ -0,0 +1,29 @@
+//
+//  CLViewController.m
+//  Clare
+//
+//  Created by workpanda on 03/19/2020.
+//  Copyright (c) 2020 workpanda. All rights reserved.
+//
+
+#import "CLViewController.h"
+
+@interface CLViewController ()
+
+@end
+
+@implementation CLViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
