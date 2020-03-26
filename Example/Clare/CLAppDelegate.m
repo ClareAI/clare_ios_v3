9eca2ee395336ce5f962ebf5a33cf2356813221f
Ying Yu <vernal@clare.ai>
2020-03-26 13:50:44 +0800

    3.1.25

--- Example/Clare/CLAppDelegate.m
+++ Example/Clare/CLAppDelegate.m
@@ -13,7 +13,91 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    
+    CLSettings *settings = [CLSettings initWithAppId:@"5e158202870229000f7b7a12"];
+    //settings.host = @"http://10.0.1.23:5500";
+    settings.host = @"https://api-staging.clare.ai";
+    
+    settings.titles = [[NSMutableDictionary alloc]init];
+    [settings.titles setObject:@"Clare Assistant" forKey:@"en_US"]; //1
+    [settings.titles setObject:@"Clare 聊天機械人" forKey:@"zh_HK"];
+    [settings.titles setObject:@"Clare 聊天机器人" forKey:@"zh_CN"];
+    settings.themeColor =  [UIColor colorWithRed:182.0/255.0 green:141.0/255.0 blue:60.0/255.0 alpha:1.0]; //2
+
+    settings.disclaimers = [[NSMutableDictionary alloc]init];
+    [settings.disclaimers setObject:@[@"<span>• Virtual Assistant is an automated service, and therefore cannot answer questions specific to your account.<br>• Please do NOT disclose any personal account information in your messages.<br></span>"] forKey:@"en_US"];//4   //,@"<span>• The information you input here will be recorded for service quality assurance.<br>• Data will be processed and stored by members of the Fidelity Group and authorised third parties, which may include processing overseas.<br>• For the details please read the <a href=\"hktv://www.hktvmall.com/p/H0883004_S_10214181\">Security and Privacy</a> guidelines.</span>"
+    [settings.disclaimers setObject:@[@"<span>• 虛擬助理是全自動化服務，因此無法回答有關您的帳戶資料的問題。<br>• 請勿在您的訊息中透露任何個人帳戶資料。<br></span>"] forKey:@"zh_HK"];//,@"<span>• 富達將儲存您在此提供的資料，以確保服務質素。<br>• 富達集團成員及獲授權第三方將處理及儲存資料，當中可能包括海外處理。<br>• 詳情請細閱<a href=\"https://www.hktvmall.com/p/H0883004_S_10214181\">安全與私隱。</a></span>"
+    [settings.disclaimers setObject:@[@"<span>• 虚拟助理是全自动化服务，因此无法回答有关您的帐户资料的问题。<br></span>",@"富达将储存您在此提供的资料，以确保服务质素。"] forKey:@"zh_CN"];
+    
+    settings.languages = [[NSArray alloc]initWithObjects: @"en_US",nil]; //5
+    settings.microphoneEnable = YES; //6
+    settings.voiceEnable = NO; //7
+    settings.isReplyButtonInOneRow = NO; //8
+    settings.loadHistory = YES; //9
+    settings.languageDetection = YES; //10
+    settings.sendGreetingAgain = YES; // 11
+    
+    settings.allowUploadFile = NO;
+    
+    CLStyleSetting* styleSetting = [CLStyleSetting new];
+    styleSetting.chatBgColor = UIColor.whiteColor;
+    styleSetting.linkColor = UIColor.blueColor;
+    styleSetting.quickReplyButtonBgColor = UIColor.cyanColor;
+    styleSetting.quickReplyButtonFontColor = UIColor.whiteColor;
+    styleSetting.quickReplyButtonBorderColor = UIColor.blueColor;
+    styleSetting.bubbleFontColor = UIColor.whiteColor;
+    styleSetting.bubbleBgColor = UIColor.whiteColor;
+    styleSetting.bubbleBorder = YES;
+    styleSetting.bubbleFontColor = UIColor.blackColor;
+    styleSetting.headerBarBgColor = UIColor.blueColor;
+    styleSetting.userBubbleBgColor = UIColor.whiteColor;
+    styleSetting.userBubbleBorder = YES;
+    styleSetting.userBubbleFontColor = UIColor.blueColor;
+    styleSetting.postbackBgColor = UIColor.blackColor;
+    styleSetting.postbackBorder = YES;
+    styleSetting.postbackFontColor = UIColor.whiteColor;
+    styleSetting.searchResultFontColor = UIColor.blueColor;
+    
+    [[Clare sharedManager] setStyle:styleSetting];
+    
+    settings.languageDetection = true;
+    
+    [[Clare sharedManager] init:settings withCompletion:^(BOOL ignored,BOOL success, NSError* _Nullable error) {
+        if(ignored){
+            NSLog(@"already init, not need re-init");
+        }else{
+            NSLog(@"userid sessionid %@",[[Clare sharedManager]getUserSession]);
+            if (success) {
+                NSMutableDictionary *properties = [[NSMutableDictionary alloc]init];
+                [properties setObject:@"Kowloon" forKey:@"location"];
+                [properties setObject:@"10000" forKey:@"age"];
+                [[Clare sharedManager]updateUserProperties:properties completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+                    NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *) response;
+                    NSLog(@"response status code: %ld", (long)[httpResponse statusCode]);
+                    NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:(NSJSONReadingMutableLeaves) error:nil];
+                    NSLog(@"response %@",dict);
+                    if(error){
+                        NSLog(@"update_error %@", error);
+                    }
+                }];
+            }
+            if (error) {
+                NSLog(@"init_error1 %@",error);
+            }
+        }
+        
+    }];
+    
+    [Clare sharedManager].conversationDelegate = self;
+    [Clare sharedManager].chatBotDelegate = self;
+    
+    NSSetUncaughtExceptionHandler(&uncaughtExceptionHandler);
+    
     return YES;
+}
+
+static void uncaughtExceptionHandler(NSException *exception) {
+    NSLog(@"%@\n%@", exception, [exception callStackSymbols]);
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
@@ -41,6 +125,28 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+
+#pragma mark - Core Data Saving support
+
+- (void)saveContext {
+    NSManagedObjectContext *context = self.persistentContainer.viewContext;
+    NSError *error = nil;
+    if ([context hasChanges] && ![context save:&error]) {
+        // Replace this implementation with code to handle the error appropriately.
+        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+        NSLog(@"Unresolved error %@, %@", error, error.userInfo);
+        abort();
+    }
+}
+
+- (void)onUnreadCountChanged:(int)count {
+    NSLog(@"未读 %d",count);
+}
+
+-(void)onChotBotClosed{
+    NSLog(@"ChatBot=", @"Closed");
 }
 
 @end
