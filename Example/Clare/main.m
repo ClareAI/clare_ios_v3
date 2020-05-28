9e34a2db61f8d8ee62671c3245e1efe872f83ca2
Ying Yu <vernal@clare.ai>
2020-05-28 21:29:19 +0800

    Initial commit

---
+++ Example/Clare/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Clare
+//
+//  Created by vernalclareai on 05/28/2020.
+//  Copyright (c) 2020 vernalclareai. All rights reserved.
+//
+
+@import UIKit;
+#import "CLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CLAppDelegate class]));
+    }
+}
