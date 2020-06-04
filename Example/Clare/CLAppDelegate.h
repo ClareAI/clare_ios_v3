3a98abf6485df7fcabf126fb4aa162021c85fa65
Ying Yu <vernal@clare.ai>
2020-06-04 17:40:21 +0800

    3.2.6

--- Example/Clare/CLAppDelegate.h
+++ Example/Clare/CLAppDelegate.h
@@ -6,10 +6,18 @@
 //  Copyright (c) 2020 vernalclareai. All rights reserved.
 //
 
-@import UIKit;
 
-@interface CLAppDelegate : UIResponder <UIApplicationDelegate>
+@import UIKit;
+#import <CoreData/CoreData.h>
+#import <Clare/Clare.h>
+
+@interface CLAppDelegate : UIResponder <UIApplicationDelegate,ConversationDelegate,ChatBotDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
 @end
+
