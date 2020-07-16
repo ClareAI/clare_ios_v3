228aa1d2594dd3f00874cd22eaab688ab0571b94
Ying Yu <vernal@clare.ai>
2020-07-16 11:16:35 +0800

    3.2.12

--- Example/Clare/CLViewController.m
+++ Example/Clare/CLViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "CLViewController.h"
+#import <Clare/Clare.h>
 
 @interface CLViewController ()
 
@@ -17,7 +18,13 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    
+    [[Clare sharedManager] show];
 }
 
 - (void)didReceiveMemoryWarning
