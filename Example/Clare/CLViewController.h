a30948759284740808a57eee52eff1ed6b759e8a
Ying Yu <vernal@clare.ai>
2020-08-05 22:34:06 +0800

    Initial commit

---
+++ Example/Clare/CLViewController.h
@@ -0,0 +1,13 @@
+//
+//  CLViewController.h
+//  Clare
+//
+//  Created by vernalclareai on 08/05/2020.
+//  Copyright (c) 2020 vernalclareai. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CLViewController : UIViewController
+
+@end
