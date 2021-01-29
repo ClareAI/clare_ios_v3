0f9fb97b50750b3cee7b5357237791eb3abefd5a
Ying Yu <vernal@clare.ai>
2021-01-29 17:02:52 +0800

    Initial commit

---
+++ Example/Clare/CLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CLAppDelegate.h
+//  Clare
+//
+//  Created by vernalclareai on 01/29/2021.
+//  Copyright (c) 2021 vernalclareai. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
